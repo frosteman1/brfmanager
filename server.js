20614c23b8619339b485ed4009a4ee4e9b2cf52c
Johannes Frosteman <johannesfrosteman@MacBook-Air-som-tillhor-Johannes.local>
2025-02-18 15:57:51 +0100

    Ditt beskrivande commit-meddelande

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
