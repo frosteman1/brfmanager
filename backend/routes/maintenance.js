d8393d1142affd67c99861f2754b449d0d18bc9f
Johannes Frosteman <johannesfrosteman@MacBook-Air-som-tillhor-Johannes.local>
2025-02-18 17:04:53 +0100

    Ditt beskrivande commit-meddelande

--- backend/routes/maintenance.js
+++ backend/routes/maintenance.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const MaintenanceItem = require('../models/MaintenanceItem');
+const MaintenanceItem = require('../models/maintenanceitem');
 
 // GET maintenance items
 router.get('/', auth, async (req, res) => {
